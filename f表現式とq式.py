57613960439c0dbb76008a3f9c6e36f9c989689a
Syoitu Den <42715311+huanshenyi@users.noreply.github.com>
2019-02-08 18:34:23 +0900

    Update f表現式とq式.py

--- f表現式とq式.py
+++ f表現式とq式.py
@@ -54,3 +54,30 @@ def index7(request):
       print(book.name, book.price, book.rating)
 
     return HttpResponse("index7")
+
+
+
+def index2(request):
+    """id>2の本"""
+    # books = Book.objects.filter(id__gte=2)
+
+    """id !=3 の本"""
+    # books =Book.objects.filter(~Q(id=3))
+
+    """[filter(~Q(id=3))] id >= 2 && id != 3  链式调用"""
+    # books = Book.objects.filter(id__gte=2).filter(~Q(id=3))
+
+    """[exclude]条件付きで排除"""
+    # books = Book.objects.filter(id__gte=2).exclude(id=3)
+
+    """本を選択してかつ作者名を結果に追加"""
+    # books = Book.objects.filter(id=2).annotate(author_name=F('author__name'))
+    # for boos in books:
+    #     print(boos.name,boos.author_name)
+    """本と作者を表示"""
+    books = Book.objects.annotate(author_name=F('author__name'))
+    for book in books:
+     print(book.name, book.author_name)
+
+    print(connection.queries)
+    return HttpResponse('index2')
